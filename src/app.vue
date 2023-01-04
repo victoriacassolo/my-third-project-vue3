7a55707e99c07c7cbf643c66da92582d9271ef27
victoriacassolo <vitoria.cassolo@gmail.com>
2023-01-04 17:49:09 -0300

    feat: new component created

--- src/app.vue
+++ src/app.vue
@@ -1,6 +1,7 @@
 <script setup>
 import q from './data/quizes.json';
 import { ref, watch } from 'vue';
+import Card from './components/Card.vue';
 
 const quizes = ref(q);
 const search = ref('');
@@ -19,13 +20,7 @@ watch(search, () => {
       <input v-model.trim="search" type="text" placeholder="search..." />
     </header>
     <div class="options-container">
-      <div v-for="quiz in quizes" :key="quiz.id" class="card">
-        <img :src="quiz.img" alt="" />
-        <div class="card-text">
-          <h2>{{ quiz.name }}</h2>
-          <p>{{ quiz.questions.length }} questions</p>
-        </div>
-      </div>
+      <Card v-for="quiz in quizes" :key="quiz.id" :quiz="quiz" />
     </div>
   </div>
 </template>
@@ -60,31 +55,5 @@ header input {
   display: flex;
   flex-wrap: wrap;
   margin-top: 4px;
-}
-
-/* CARD */
-
-.card {
-  width: 310px;
-  overflow: hidden;
-  border-radius: 2%;
-  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
-  margin-bottom: 35px;
-  margin-right: 20px;
-  cursor: pointer;
-}
-
-.card img {
-  width: 100%;
-  height: 190px;
-  margin: 0;
-}
-
-.card .card-text {
-  padding: 0 5px;
-}
-
-.card .card-text h2 {
-  font-weight: bold;
 }
 </style>

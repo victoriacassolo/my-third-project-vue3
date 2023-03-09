61e20cb23cf4919f52e02f792c2fd0a73348b70f
victoriacassolo <vitoria.cassolo@gmail.com>
2023-03-09 12:18:56 -0300

    feat: add percentage bar

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -1,96 +1,51 @@
+<script setup>
+import Question from '../components/Question.vue';
+import QuizHeader from '../components/QuizHeader.vue';
+import { useRoute } from 'vue-router';
+import { ref, watch, computed } from 'vue';
+import quizes from '../data/quizes.json';
+
+const route = useRoute();
+const quizId = parseInt(route.params.id);
+const quiz = quizes.find((q) => q.id === quizId);
+const currentQuestionIndex = ref(0);
+
+// const questionStatus = ref(
+//   `${currentQuestionIndex.value}/${quiz.questions.length}`
+// );
+// watch(
+//   () => currentQuestionIndex.value,
+//   () => {
+//     questionStatus.value = `${currentQuestionIndex.value}/${quiz.questions.length}`;
+//   }
+// );
+
+const questionStatus = computed(
+  () => `${currentQuestionIndex.value}/${quiz.questions.length}`
+);
+
+const barPercentage = computed(
+  () => `${(currentQuestionIndex.value / quiz.questions.length) * 100}%`
+);
+</script>
+
 <template>
   <div>
-    <header>
-      <h4>Question 1/3</h4>
-      <div class="bar">
-        <div class="completion"></div>
-      </div>
-    </header>
-  </div>
-  <div class="question-container">
-    <h1 class="question">What is the chemical value of table salt</h1>
-  </div>
-  <div class="options-container">
-    <div class="option">
-      <p class="option-label">A</p>
-      <div class="option-value">
-        <p>NaCl</p>
-      </div>
-    </div>
-    <div class="option">
-      <p class="option-label">B</p>
-      <div class="option-value">
-        <p>NaCl</p>
-      </div>
-    </div>
-    <div class="option">
-      <p class="option-label">C</p>
-      <div class="option-value">
-        <p>NaCl</p>
-      </div>
-    </div>
-    <div class="option">
-      <p class="option-label">D</p>
-      <div class="option-value">
-        <p>NaCl</p>
-      </div>
-    </div>
+    <QuizHeader
+      :questionStatus="questionStatus"
+      :barPercentage="barPercentage"
+    />
+    <div><Question :question="quiz.questions[currentQuestionIndex]" /></div>
+    <button
+      @click="
+        {
+          currentQuestionIndex < quiz.questions.length
+            ? currentQuestionIndex++
+            : (button.disabled = true);
+        }
+      "
+    >
+      Next Question
+    </button>
   </div>
 </template>
-
-<style scoped>
-header {
-  margin-top: 20px;
-}
-
-header h4 {
-  font-size: 30px;
-}
-
-.bar {
-  width: 300px;
-  height: 50px;
-  border: 3px solid bisque;
-}
-
-.completion {
-  height: 100%;
-  width: 0%;
-  background-color: bisque;
-}
-
-.question-container {
-  margin-top: 20px;
-}
-
-.question {
-  font-size: 40px;
-
-  margin-bottom: 20px;
-}
-
-.option {
-  display: flex;
-  color: black;
-  margin-bottom: 20px;
-  cursor: pointer;
-}
-
-.option-label {
-  background-color: bisque;
-  color: black;
-  width: 60px;
-  height: 60px;
-  font-size: 30px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.option-value {
-  background-color: rgb(244, 239, 239);
-  width: 100%;
-  font-size: 30px;
-  padding: 0 20px;
-}
-</style>
